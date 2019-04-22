d2e02f4070f9bfb61056c00c97c99bee75431cae
jarvis <jiashunming@outlook.com>
2019-04-22 21:21:02 +0800

    feat: 添加范型的默认处理

--- packages/cache-intl/src/index.ts
+++ packages/cache-intl/src/index.ts
@@ -1,4 +1,4 @@
-export default interface CacheIntl<T extends any> {
+export default interface CacheIntl<T extends any = any> {
   get(key: string): T
   set(key: string, value: T): void
   has(key: string): boolean
